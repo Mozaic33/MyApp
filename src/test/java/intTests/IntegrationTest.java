f925712f5a2ecde1054e25b0ca459f23611beeea
Dumitru Neacsu <Dumitru.Neacsu@endava.com>
2018-11-23 17:22:36 +0200

    update for docker

--- src/test/java/intTests/IntegrationTest.java
+++ src/test/java/intTests/IntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.experimental.categories.Category;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.testng.Assert;
+import org.testng.TestRunner;
 
 import java.io.File;
 @Category(IntegrationTest.class)
@@ -14,7 +15,7 @@ public class IntegrationTest {
 
     public void TestTitle1() {
 
-        File src = new File("/usr/local/bin/phantomjs");
+        File src = new File("C:/Users/dneacsu/Desktop/phantomjs-2.1.1-windows/bin/phantomjs.exe");
         System.setProperty("phantomjs.binary.path", src.getAbsolutePath());
         WebDriver driver = new PhantomJSDriver();
         driver.get("http://192.168.39.31:8989/AMGrads");
---
+++ src/test/java/intTests/IntegrationTest2.java
@@ -0,0 +1,27 @@
+package intTests;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.testng.Assert;
+
+import java.io.File;
+
+@Category(IntegrationTest2.class)
+
+public class IntegrationTest2 {
+    @Test
+
+    public void TestTitle1() {
+
+        File src = new File("C:/Users/dneacsu/Desktop/phantomjs-2.1.1-windows/bin/phantomjs.exe");
+        System.setProperty("phantomjs.binary.path", src.getAbsolutePath());
+        WebDriver driver = new PhantomJSDriver();
+        driver.get("http://192.168.39.32:8080/AMGrads");
+        String title1 = driver.getTitle();
+        Assert.assertTrue(title1.contains("MyApp"));
+
+
+    }
+}
